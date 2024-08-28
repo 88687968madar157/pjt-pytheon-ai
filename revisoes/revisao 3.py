2a8e8903b9474a6e089ec88882b6406fb48d338b
Kawan Barbosaa <kawanbarbosa076@gmail.com>
2024-08-28 17:41:38 -0300

    Aula de revisao

---
+++ revisoes/revisao 3.py
@@ -0,0 +1,8 @@
+idade = 18
+if(idade>=18):
+    print("adulto")
+else:
+    if(idade>=18):
+        print("adolecente")
+    else:
+        print("criança")
---
+++ revisoes/revisão 2.py
@@ -0,0 +1,5 @@
+idade = 16
+if(idade>=18):
+    print("maior de idede")
+else:
+    print("menor de idade")
