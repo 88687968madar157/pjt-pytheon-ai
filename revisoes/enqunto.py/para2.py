c9f0837615c63d15fe8cc7d0bbfdb646e9e9130c
Kawan Barbosaa <kawanbarbosa076@gmail.com>
2024-08-28 18:59:22 -0300

    laços de repitição

---
+++ revisoes/enqunto.py/para2.py
@@ -0,0 +1,18 @@
+alunos = [
+    "kawan",
+    "kaike",
+    "maria",
+    "marlon",
+    "karina",
+    "jonata",
+    "gilherme",
+    "joao",
+    "pedro"
+]
+
+alunos.sort()
+alunos.append("vitoria")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
